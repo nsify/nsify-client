268842b882503d2a5bb7f86e923748797d8f7ba4
Maik de Kruif <maikka39@gmail.com>
2019-06-09 14:33:59 +0200

    Now showing recieved data

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,6 +8,15 @@
       v-model="search"
       placeholder="Location">
 
+    <!-- <ul v-html="stations"> -->
+      <li v-for="(station, index) in stations" :key="`station-${index}`">
+        <strong>{{station.name}}</strong>
+        <span>{{station.city}}</span>
+        <span>{{station.province}}</span>
+      </li>
+    <!-- </ul> -->
+    <div class="error" v-html="error"></div>
+
   </div>
 </template>
 
@@ -19,7 +28,9 @@ let searchTimeout
 export default {
   data () {
     return {
-      search: ''
+      search: '',
+      stations: null,
+      error: null
     }
   },
   watch: {
@@ -27,30 +38,55 @@ export default {
       // Don't yet send the location to the server if
       // the user is still typing
       clearTimeout(searchTimeout)
-      searchTimeout = setTimeout(() => { searchStation(value) }, 250)
+      searchTimeout = setTimeout(async () => {
+        let tmp = await searchStation(value)
+        console.log(tmp)
+        this.stations = tmp
+        // this.error = tmp.stations.Tiel
+      }, 250)
     }
+  },
+  methods: {
+
   }
 }
 
 /**
- * Request a station from the server
- * @param {string} location - Location specified by the user.
- */
+* Request a station from the server
+* @param {string} location - Location specified by the user.
+* @return {Object} - Nearby stations
+*/
 async function searchStation (location) {
   if (location === '') {
-    return
+    return {}
   }
-  console.log('Searching for station...')
-  let response = await StationSearchService.search({
-    location: location
-  })
-  console.log(response.data)
+
+  let response
+
+  console.log('Searching for stations...')
+  try {
+    response = await StationSearchService.search({
+      location: location
+    })
+    return response.data.stations
+  } catch (e) {
+    return {error: 'Autofill unavailable...'}
+  } finally {
+
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 input {
   font-size: 1.5em;
+}
+
+.error {
+  margin-top: 1em;
+  font-size: 1.1em;
+  color: red;
 }
 </style>
